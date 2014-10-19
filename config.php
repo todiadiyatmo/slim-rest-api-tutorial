0cb7f03b3e7f635da8c2ed64423a7ebd2b5ae3e2
Todiadiyatmo <todiadiyatmo@tonjoo.com>
2014-10-19 16:32:02 +0700

    readme

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['username'] = 'root';
+$config['password'] = '';
+$config['host'] = 'localhost';
+$config['database'] = 'layanan_web';
+
+$config['api_url'] = 'http://127.0.0.1/todi/slim-rest-api-tutorial/api/index.php';
