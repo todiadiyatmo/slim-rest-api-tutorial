301330444b151c9408753e6b3a052e681925bab6
Todiadiyatmo <todiadiyatmo@tonjoo.com>
2014-10-19 13:02:46 +0700

    fix config location

--- api/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$database['username'] = 'root';
-$database['password'] = 'vagrant';
-$database['host'] = 'localhost';
-$database['database'] = 'layanan_web';
--- client/index.php
+++ client/index.php
@@ -7,10 +7,31 @@ $app = new \Slim\Slim(array(
     'templates.path' => './view'
 ));
  
-
  // Default route
 $app->get('/', function(){
     echo "Welcome to Slim";
 }); 
+
+ // Default route
+$app->get('/cities', function(){
+
+	$headers = array('Accept' => 'application/json');
+	$options = array('auth' => array('user', 'pass'));
+	$request = Requests::get('https://api.github.com/gists', $headers, $options);
+
+	var_dump($request->status_code);
+	// int(200)
+
+	var_dump($request->headers['content-type']);
+	// string(31) "application/json; charset=utf-8"
+
+	var_dump($request->body);
+	// string(26891) "[...]"
+    
+}); 
+
+
+
+
  
 $app->run();
