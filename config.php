7243a5094be39706254c87c7232fe9a037a27eaa
todiadiyatmo <todiadiyatmo@tonjoo.com>
2014-10-21 09:27:34 +0700

    config remove

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config['username'] = 'root';
-$config['password'] = '';
-$config['host'] = 'localhost';
-$config['database'] = 'layanan_web';
-
-$config['api_url'] = 'http://127.0.0.1/todi/slim-rest-api-tutorial/api/index.php';
